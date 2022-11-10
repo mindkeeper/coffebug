e638971890590e00937a73d1041aa1b7cae91b8a
mindkeeper <fcb.nyak@gmail.com>
2022-11-11 02:39:41 +0700

    add reset password route

--- src/routes/auths.js
+++ src/routes/auths.js
@@ -7,5 +7,6 @@ const allowed = { login: ["email", "password"] };
 const isLogin = require("../middleware/isLogin");
 authsRouter.post("/login", validate.body(...allowed.login), authsHandler.login);
 authsRouter.delete("/logout", isLogin(), authsHandler.logout);
+authsRouter.post("/reset", authsHandler.resetPassword);
 
 module.exports = authsRouter;
